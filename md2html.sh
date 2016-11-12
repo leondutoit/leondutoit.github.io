9a4c08ebdf4a57eb4b24e38cc9d417d40019d19d
leondutoit <dutoit.leon@gmail.com>
2016-11-12 13:19:57 +0100

    better html file generation

--- md2html.sh
+++ md2html.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-usage="./md2html.sh infile.md"
+if [ $# -lt 1 ]; then
+    printf "\n    Missing arguments, exiting"
+    printf "\n    For help do: ./md2html.sh -h or ./md2html.sh --help\n\n"
+    exit 1
+fi
 
-infile=$1
-elements=(${infile//./ })
-outfile=$(echo ${elements[0]} | sed 's/posts\///')
+usage="\
 
-pandoc -s -c tufte.css -o $outfile.html $infile
+    Usage:
+
+    ./md2html.sh --input <posts, writings>/infile.md
+"
+
+create_output_file() {
+    infile=$1
+    elements=(${infile//./ })
+    outfile=$(basename $elements)
+    pandoc -s -c tufte.css -o $outfile.html $infile
+}
+
+while (( "$#" )); do
+    case $1 in
+        --input)        shift; create_output_file $1; shift ;;
+        -h | --help)    printf "%s\n" "$usage"; exit 0 ;;
+        *) break ;;
+    esac
+done
